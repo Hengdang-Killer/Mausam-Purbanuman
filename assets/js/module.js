f2bc9b195326cfc3d162d007a6469594c151008b
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2023-07-31 16:03:56 +0530

    Update module.js

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,4 +1,17 @@
+/*-------------------------------------FUNCTION OF THIS FILE-------------------------------------*/
+/*
+This file contains several utility functions and data related to weather and air quality. These utility functions and data are useful for formatting date and time strings and for converting speed units. The aqiText object provides descriptions of air quality levels based on the AQI scale, helping users understand the air quality conditions in a user-friendly way.
+*/ 
+
+
 "use strict";
+
+
+
+/*------------------------------ARRAY OF WEEK AND MONTHS FOR MAPPING-------------------------------------*/
+/*
+These are arrays containing names of weekdays and months, respectively. These arrays are used to format date and time strings later in the code.
+*/
 
 export const weekDayNames = [
   "Sunday",
@@ -25,6 +38,13 @@ export const monthNames = [
   "Dec",
 ];
 
+
+
+
+/*---------------------------------------------GETTING THE CURRENT DATE-------------------------------------*/
+/*
+This function takes a Unix date in seconds and a timezone shift from UTC in seconds as input. It converts the Unix date to a human-readable date string in the format "Sunday 10, Jan" (e.g., "Sunday 10, Feb"). It uses the weekDayNames and monthNames arrays to get the names of the weekday and month corresponding to the provided date.
+*/
 /**
  *
  * @param {number} dateUnix Unix date in seconds
@@ -41,6 +61,12 @@ export const getData = function (dateUnix, timezone) {
 
 };
 
+
+
+/*---------------------------------------------GETTING THE CURRENT TIME-------------------------------------*/
+/*
+This function takes a Unix time in seconds and a timezone shift from UTC in seconds as input. It converts the Unix time to a human-readable time string in the format "HH:MM AM/PM" (e.g., "02:30 PM"). The function calculates the hours and minutes from the provided time and also determines whether it is "AM" or "PM" based on the hour.
+*/
 /**
  *
  * @param {number} dateUnix Unix date in seconds
@@ -58,6 +84,13 @@ export const getTime = function (timeUnix, timezone) {
 
 };
 
+
+
+
+/*---------------------------------------------GETTING THE CURRENT HOUR-------------------------------------*/
+/*
+This function is similar to the getTime function but returns a time string without the minutes. The format is "HH AM/PM" (e.g., "02 PM").
+*/
 /**
  *
  * @param {number} dateUnix Unix date in seconds
@@ -74,12 +107,18 @@ export const getHours = function (timeUnix, timezone) {
 
 };
 
+
+
+
+/*---------------------------------------------CHANGING THE UNIT OF THE CURRENT WIND SPEED-------------------------------------*/
+/*
+This function converts meters per second (mps) to kilometers per hour (km/h). It takes a speed value in mps as input and returns the equivalent value in km/h.
+*/
 /**
  *
  * @param {number} mps Metter per seconds
  * @returns {number} Kilometer per hours
  */
-
 export const mps_to_kmh = (mps) => {
 
   const mph = mps * 3600;
@@ -87,6 +126,13 @@ export const mps_to_kmh = (mps) => {
 
 };
 
+
+
+
+/*---------------------------------------------SUGGESTIONS REGARDING WEATHER CONDITIONS-------------------------------------*/
+/*
+This object "aqiText" that maps Air Quality Index (AQI) levels to corresponding text descriptions. It provides information about air quality levels and their associated messages. The AQI levels range from 1 to 5, and each level has a "level" and a "message" property.
+*/
 export const aqiText = {
 
   1: {
